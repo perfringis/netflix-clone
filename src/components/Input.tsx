7895c1c7a05f43392fa9e5cb27b6026dd96b9354
perfringis <kopko.mateusz@gmail.com>
2023-06-14 13:37:43 +0200

    restyle

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,15 +4,14 @@ const Input = () => {
       <input
         id="email"
         className="
-        block
-        rounded-md
+        rounded
         px-6
         pt-6
         pb-1
         w-full
-        text-md
-      text-white
-      bg-neutral-700
+        text-base
+        text-white
+        bg-mine-shaft
         appearance-none
         focus:outline-none
         focus:ring-0
@@ -22,25 +21,25 @@ const Input = () => {
       />
       <label
         className="
-        absolute 
-        text-md
-      text-zinc-400
-        duration-150 
-        transform 
-        -translate-y-3 
-        scale-75 
-        top-4 
-        z-10 
-        origin-[0] 
+        absolute
+        text-base
+      text-gray
+        duration-150
+        transform
+        -translate-y-3
+        scale-75
+        top-4
+        z-10
+        origin-[0]
         left-6
-        peer-placeholder-shown:scale-100 
-        peer-placeholder-shown:translate-y-0 
+        peer-placeholder-shown:scale-100
+        peer-placeholder-shown:translate-y-0
         peer-focus:scale-75
         peer-focus:-translate-y-3
       "
         htmlFor="email"
       >
-        Email
+        Email or phone number
       </label>
     </div>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    fontSize: {
+      '32px': ['32px', '38px'],
+    },
+    extend: {
+      colors: {
+        "mine-shaft": "#333333",
+        gray: "#8c8c8c",
+      },
+    },
   },
   plugins: [],
 };
